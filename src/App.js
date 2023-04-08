242299d374d34a16cde15f06af2eb942eba63bd1
Amine Akacha <akachamohamedamine@gmail.com>
2023-04-08 01:47:19 +0100

    Setup bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,314 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="d-flex h-100 text-white bg-dark">
+			<div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+				<header className="mb-auto">
+					<div>
+						<img src={logo} className="float-md-start img-logo mb-0" alt="logo" />
+						<nav className="nav nav-masthead justify-content-center float-md-end">
+							<a className="nav-link active" aria-current="page" href="/">Spaceships</a>
+							<a className="nav-link" href="/">Planets</a>
+							<a className="nav-link" href="/">Films</a>
+						</nav>
+					</div>
+				</header>
+				<hr />
+				<div className="container mt-3 ">
+					<div className="row">
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-3">
+							<div className="card border-secondary  text-white bg-dark mb-3">
+								<img src="https://picsum.photos/250/100" className="card-img-top" />
+								<div className="card-body">
+									<h5 className="card-title">CR90 corvette</h5>
+									<hr />
+									<p className="card-text">
+										<ul>
+											<li>
+												Manufacturer: Corellian Engineering Corporation.
+											</li>
+											<li>
+												Passengers : 5000
+											</li>
+											<li>
+												Consumables : 1 year
+											</li>
+										</ul>
+									</p>
+									<a href="#" class="btn btn-secondary  text-white">Read more</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<hr />
+				<footer className="mt-auto text-center text-white-50">
+					<p>By <a href="https://github.com/aminehck" target="_blank" rel="noreferrer" className="text-white">@Aminehck</a>, Data from <a href="https://swapi.dev/" target="_blank" rel="noreferrer" className="text-white">SWAPI</a>.</p>
+				</footer>
+			</div>
+		</div>
+	);
 }
 
 export default App;
